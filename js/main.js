358c18863dbce2ea411eb0a976002c51863c6805
Greg Ells <gregells@gmail.com>
2025-02-27 19:18:02 -0500

    Reduce message length

--- js/main.js
+++ js/main.js
@@ -297,7 +297,7 @@ function renderMessage() {
   elements.message.innerHTML = '';
 
   // Render the number of correctly guessed words and the total possible:
-  const text = `${state.correctGuesses.length} / ${state.validWords.length} words found`;
+  const text = `${state.correctGuesses.length} / ${state.validWords.length} words`;
   messageElement = document.createElement('div');
   messageElement.innerText = text;
   elements.message.appendChild(messageElement);
